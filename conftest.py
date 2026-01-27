13cc5dfc6799fbd2464f5dd5d69156bc8c38e833
Romans <roman.veremeicik@gmail.com>
2026-01-28 00:21:58 +0100

    Fix CI: mark smoke tests correctly

--- conftest.py
+++ conftest.py
@@ -1,14 +1,10 @@
 import pytest
 from playwright.sync_api import sync_playwright
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         yield browser
         browser.close()
 
