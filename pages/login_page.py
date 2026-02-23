8c8394b860d05d5210441f1a2618307e20ed8bb1
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 17:10:18 +0100

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,27 +1,28 @@
 from playwright.sync_api import Page, expect
 from settings import BASE_URL
 
+
 class LoginPage:
+
+    USERNAME_INPUT = "#user-name"
+    PASSWORD_INPUT = "#password"
+    LOGIN_BUTTON = "#login-button"
+    ERROR_MESSAGE = "[data-test='error']"
 
     def __init__(self, page: Page):
         self.page = page
         self.base_url = BASE_URL
 
-        self.username_input = "#user-name"
-        self.password_input = "#password"
-        self.login_button = "#login-button"
-        self.error_message = "[data-test='error']"
-
     def open_login_page(self):
         self.page.goto(self.base_url)
 
     def login(self, username: str, password: str):
-        self.page.fill(self.username_input, username)
-        self.page.fill(self.password_input, password)
-        self.page.click(self.login_button)
+        self.page.fill(self.USERNAME_INPUT, username)
+        self.page.fill(self.PASSWORD_INPUT, password)
+        self.page.click(self.LOGIN_BUTTON)
 
-    def get_error_message(self):
-        return self.page.locator(self.error_message).text_content()
+    def get_error_message(self) -> str:
+        return self.page.locator(self.ERROR_MESSAGE).text_content()
 
     def expect_error_visible(self):
-        expect(self.page.locator(self.error_message)).to_be_visible()
+        expect(self.page.locator(self.ERROR_MESSAGE)).to_be_visible()
