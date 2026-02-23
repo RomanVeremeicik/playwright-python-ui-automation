690578c820cd97c9e7e7ae406d4af389aa3b0388
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 16:47:16 +0100

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,27 +1,24 @@
-from playwright.sync_api import Page
 from pages.base_page import BasePage
 from settings import BASE_URL
 
 
 class LoginPage(BasePage):
 
-    URL = BASE_URL
-
     USERNAME = "#user-name"
     PASSWORD = "#password"
     LOGIN_BUTTON = "#login-button"
     ERROR_MESSAGE = "[data-test='error']"
 
-    def __init__(self, page: Page):
-        super().__init__(page)
-
     def open(self):
-        self.page.goto(self.URL)
+        super().open(BASE_URL)
 
     def login(self, username: str, password: str):
         self.fill(self.USERNAME, username)
         self.fill(self.PASSWORD, password)
         self.click(self.LOGIN_BUTTON)
 
-    def get_error_text(self) -> str:
+    def get_error_message(self) -> str:
         return self.get_text(self.ERROR_MESSAGE)
+
+    def is_error_visible(self) -> bool:
+        return self.is_visible(self.ERROR_MESSAGE)
