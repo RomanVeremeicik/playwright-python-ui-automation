10305a4c8547f8a25a8753f5596e4a58d4dba292
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 16:30:00 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL", "https://www.saucedemo.com")
+
+STANDARD_USER = os.getenv("STANDARD_USER", "standard_user")
+PASSWORD = os.getenv("PASSWORD", "secret_sauce")
+
+HEADLESS = os.getenv("HEADLESS", "true").lower() == "true"
