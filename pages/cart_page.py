15f641a2d63809b58a780dbe0b54ffa6fb1676fb
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 14:35:21 +0100

    Update cart_page.py

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,36 +1,17 @@
+from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
 
+
 class CartPage(BasePage):
-    """
-    Cart page.
-    Responsibility:
-    - view selected items
-    - remove items
-    - navigation (back / checkout)
-    """
 
-    CART_ITEM = ".cart_item"
-    CART_ITEM_NAME = ".inventory_item_name"
-    CART_ITEM_PRICE = ".inventory_item_price"
+    def __init__(self, page: Page):
+        super().__init__(page)
 
-    REMOVE_BUTTON = "[data-test^='remove']"
-    BACK_TO_INVENTORY_BUTTON = "#continue-shopping"
-    CHECKOUT_BUTTON = "#checkout"
+        self.cart_items = page.locator(".cart_item")
+        self.checkout_button = page.locator("#checkout")
 
-    def get_items_count(self):
-        return self.page.locator(self.CART_ITEM).count()
+    def expect_not_empty(self):
+        expect(self.cart_items).not_to_have_count(0)
 
-    def get_first_item_name(self):
-        return self.page.locator(self.CART_ITEM_NAME).first.text_content()
-
-    def get_first_item_price(self):
-        return self.page.locator(self.CART_ITEM_PRICE).first.text_content()
-
-    def remove_first_item(self):
-        self.page.locator(self.REMOVE_BUTTON).first.click()
-
-    def go_back_to_inventory(self):
-        self.click(self.BACK_TO_INVENTORY_BUTTON)
-
-    def go_to_checkout(self):
-        self.click(self.CHECKOUT_BUTTON)
+    def proceed_to_checkout(self):
+        self.checkout_button.click()
