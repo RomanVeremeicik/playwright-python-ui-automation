fd8ca2c34234d8b16d11d5a83e66f2537e6c8274
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 14:19:00 +0100

    Update inventory_page.py

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,48 +1,13 @@
 from pages.base_page import BasePage
 
+
 class InventoryPage(BasePage):
-    """
-    Inventory page.
-    Responsibility:
-    - inventory visibility
-    - add / remove items
-    - cart badge state
-    """
 
-    INVENTORY_CONTAINER = ".inventory_list"
+    INVENTORY_LIST = ".inventory_list"
+    TITLE = ".title"
 
-    ADD_TO_CART_BUTTON = "[data-test^='add-to-cart']"
-    REMOVE_BUTTON = "[data-test^='remove']"
+    def is_inventory_loaded(self):
+        self.expect_visible(self.INVENTORY_LIST)
 
-    CART_BADGE = ".shopping_cart_badge"
-    CART_LINK = ".shopping_cart_link"
-
-    MENU_BUTTON = "#react-burger-menu-btn"
-    LOGOUT_LINK = "#logout_sidebar_link"
-
-    def logout(self):
-        self.click(self.MENU_BUTTON)
-        self.click(self.LOGOUT_LINK)
-
-    def is_inventory_visible(self):
-        return self.is_visible(self.INVENTORY_CONTAINER)
-
-    def add_first_item_to_cart(self):
-        self.page.locator(self.ADD_TO_CART_BUTTON).first.click()
-
-    def remove_first_item_from_cart(self):
-        self.page.locator(self.REMOVE_BUTTON).first.click()
-
-    def get_cart_badge_count(self):
-        badge = self.page.locator(self.CART_BADGE)
-        if badge.is_visible():
-            return int(badge.text_content())
-        return 0
-
-    def go_to_cart(self):
-        self.click(self.CART_LINK)
-
-    def add_items_to_cart(self, count: int):
-        buttons = self.page.locator(self.ADD_TO_CART_BUTTON)
-        for i in range(count):
-            buttons.nth(i).click()
+    def get_title(self):
+        return self.get_text(self.TITLE)
