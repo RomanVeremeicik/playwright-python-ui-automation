024987981b2c554dc17a7644bf6c3d029f8b7d66
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 16:39:12 +0100

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,28 +1,27 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 from pages.base_page import BasePage
 from settings import BASE_URL
 
 
 class LoginPage(BasePage):
 
+    URL = BASE_URL
+
+    USERNAME = "#user-name"
+    PASSWORD = "#password"
+    LOGIN_BUTTON = "#login-button"
+    ERROR_MESSAGE = "[data-test='error']"
+
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.username_input = page.get_by_role("textbox", name="Username")
-        self.password_input = page.get_by_role("textbox", name="Password")
-        self.login_button = page.get_by_role("button", name="Login")
-        self.error_message = page.locator("[data-test='error']")
-
     def open(self):
-        self.page.goto(BASE_URL)
+        self.page.goto(self.URL)
 
     def login(self, username: str, password: str):
-        self.username_input.fill(username)
-        self.password_input.fill(password)
-        self.login_button.click()
-
-    def expect_error_visible(self):
-        expect(self.error_message).to_be_visible()
+        self.fill(self.USERNAME, username)
+        self.fill(self.PASSWORD, password)
+        self.click(self.LOGIN_BUTTON)
 
     def get_error_text(self) -> str:
-        return self.error_message.text_content()
+        return self.get_text(self.ERROR_MESSAGE)
