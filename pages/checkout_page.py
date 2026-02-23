577d17bd250e77f2188c633695727a1c6809657e
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 14:35:56 +0100

    Update checkout_page.py

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,41 +1,27 @@
+from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
 
 
 class CheckoutPage(BasePage):
-    """
-    Checkout page.
-    Responsibility:
-    - filling checkout form
-    - finishing order
-    """
 
-    FIRST_NAME_INPUT = "#first-name"
-    LAST_NAME_INPUT = "#last-name"
-    POSTAL_CODE_INPUT = "#postal-code"
+    def __init__(self, page: Page):
+        super().__init__(page)
 
-    CONTINUE_BUTTON = "#continue"
-    FINISH_BUTTON = "#finish"
+        self.first_name = page.locator("#first-name")
+        self.last_name = page.locator("#last-name")
+        self.postal_code = page.locator("#postal-code")
+        self.continue_button = page.locator("#continue")
+        self.finish_button = page.locator("#finish")
+        self.success_message = page.locator(".complete-header")
 
-    COMPLETE_HEADER = ".complete-header"
-    ERROR_MESSAGE = "[data-test='error']"
-    CANCEL_BUTTON = "#cancel"
+    def fill_information(self, first: str, last: str, postal: str):
+        self.first_name.fill(first)
+        self.last_name.fill(last)
+        self.postal_code.fill(postal)
+        self.continue_button.click()
 
-    def fill_checkout_form(self, first_name, last_name, postal_code):
-        self.fill(self.FIRST_NAME_INPUT, first_name)
-        self.fill(self.LAST_NAME_INPUT, last_name)
-        self.fill(self.POSTAL_CODE_INPUT, postal_code)
+    def finish(self):
+        self.finish_button.click()
 
-    def continue_checkout(self):
-        self.click(self.CONTINUE_BUTTON)
-
-    def finish_checkout(self):
-        self.click(self.FINISH_BUTTON)
-
-    def is_order_completed(self):
-        return self.is_visible(self.COMPLETE_HEADER)
-
-    def is_error_displayed(self):
-        return self.is_visible(self.ERROR_MESSAGE)
-
-    def cancel_checkout(self):
-        self.click(self.CANCEL_BUTTON)
+    def expect_success(self):
+        expect(self.success_message).to_have_text("Thank you for your order!")
