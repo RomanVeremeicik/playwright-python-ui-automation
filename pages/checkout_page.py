05f7373ae163ea7a7e9715accaf923a809dd40bd
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 17:28:35 +0100

    Update checkout_page.py

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page, expect
 
+
 class CheckoutPage:
 
     FIRST_NAME_INPUT = "#first-name"
@@ -14,14 +15,13 @@ class CheckoutPage:
     def __init__(self, page: Page):
         self.page = page
 
-    def fill_checkout_information(self, first_name: str, last_name: str, postal_code: str):
+    def fill_checkout_form(self, first_name: str, last_name: str, postal_code: str):
         self.page.fill(self.FIRST_NAME_INPUT, first_name)
         self.page.fill(self.LAST_NAME_INPUT, last_name)
         self.page.fill(self.POSTAL_CODE_INPUT, postal_code)
-        self.page.click(self.CONTINUE_BUTTON)
 
-    def fill_checkout_form(self, first_name: str, last_name: str, postal_code: str):
-        self.fill_checkout_information(first_name, last_name, postal_code)
+    def continue_checkout(self):
+        self.page.click(self.CONTINUE_BUTTON)
 
     def finish_checkout(self):
         self.page.click(self.FINISH_BUTTON)
