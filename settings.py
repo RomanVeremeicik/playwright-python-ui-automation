5c3c51363e77b65d14f95748791565f2f5432142
Romans <107639210+RomanVeremeicik@users.noreply.github.com>
2026-02-23 16:48:48 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_URL = os.getenv("BASE_URL", "https://www.saucedemo.com")
+BASE_URL = os.getenv("BASE_URL", "https://www.saucedemo.com/")
 
 STANDARD_USER = os.getenv("STANDARD_USER", "standard_user")
+LOCKED_USER = os.getenv("LOCKED_USER", "locked_out_user")
 PASSWORD = os.getenv("PASSWORD", "secret_sauce")
 
 HEADLESS = os.getenv("HEADLESS", "true").lower() == "true"
